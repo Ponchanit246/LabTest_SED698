4c16d8fabd79d5c78af24b6e1a6358038ac1512c
Ponchanit246 <161193779+Ponchanit246@users.noreply.github.com>
2024-11-10 13:22:18 +0700

    Add files via upload

--- app_predict_penguin_66130701723.py
+++ app_predict_penguin_66130701723.py
@@ -1,6 +1,7 @@
 
 import numpy as np 
 import pandas as pd 
+import streamlit as st
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import LabelEncoder
